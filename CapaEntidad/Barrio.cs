dc6f1436fe06bda5f9f3420002056b3f863d6b83
Berserky <jmontana18@estudiantes.areandina.edu.co>
2024-08-25 01:19:56 -0500

    Segundo Pull, Se crean 3 nuevas Capas (Datos/Entidad/Negocio), en la capa entidad se crean las diferentes clases que estan asociadas a las tablas creadas en la base de datos, se configura el Layout con las vistas de los items para la capa de Admin

---
+++ CapaEntidad/Barrio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Barrio
+    {
+        public int IdBarrio { get; set; }
+        public string Descripcion { get; set; }
+        public Ciudad oCiudad { get; set; }
+        public Departamento oDepartameto { get; set; }
+    }
+}
---
+++ CapaEntidad/Ciudad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Ciudad
+    {
+        public int IdCiudad { get; set; }
+        public string Descripcion { get; set; }
+        public Departamento oDepartamento { get; set; }
+    }
+}
